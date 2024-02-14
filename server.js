9284bd246938c25c6c4319fc77d3bb16d8c1ed69
malikmsohaib <sobimalik9@gmail.com>
2024-02-15 03:34:42 +0500

    new changes fixed

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 text-align="center;" style="color:green;">My Java Home Tech App - version-10!!</h1> \n <h2 text-align="center;" style="color:blue;">Welcome to Java Home-Tech - Javaapp!!</h2> \n <h3 text-align="center;" style="color:red;">Java Home Tech App - version-10!!</h3> \n');
+  res.send('<h1 style="color:green;">My Java Home Tech App - version-10!!</h1> \n <h2 style="color:blue;">Welcome to Java Home-Tech - Javaapp!!</h2> \n <h3 style="color:red;">Java Home Tech App - version-10!!</h3> \n');
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
