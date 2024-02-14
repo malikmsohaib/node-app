2e974c22f8cc6de73e23bcfa77bc7532ca661407
malikmsohaib <sobimalik9@gmail.com>
2024-02-15 02:49:44 +0500

    new changes color and headings

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">My Java Home Tech App - version-10!!</h1> \n');
+  res.send('<h1 style="color:green;">My Java Home Tech App - version-10!!</h1> \n <h2 style="color:blue;">Welcome to Java Home-Tech - Javaapp!!</h2> \n <h3 style="color:red;">Java Home Tech App - version-10!!</h3> \n');
 });
 
 app.listen(PORT, HOST);
